2e16dfc52c6ee4db038dafeb795843049871a230
Platonenkov <Platonenkov87@gmail.com>
2020-02-15 16:52:52 +0300

    init

---
+++ MailAdressParser/Classes/Recipient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailAdressParser
+{
+    /// <summary>
+    /// получатель писем
+    /// </summary>
+    [Serializable]
+    public class Recipient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Company { get; set; }
+        public string Occupation { get; set; }
+        public string INN { get; set; }
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public bool WasSent { get; set; }
+
+        public DateTime? SendDateTime { get ; set ; }
+
+    }
+}
---
+++ MailAdressParser/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ClosedXML.Excel;
+
+namespace MailAdressParser
+{
+    class Program
+    {
+        public static void ReadExcel(string filePath, string fileSave)
+        {
+            RecipientsList RecipentList = new RecipientsList();
+            if(!File.Exists(filePath))return;
+            // начало использования библиотеке ClosedXML
+            var workbook = new XLWorkbook(filePath);
+            var worksheet = workbook.Worksheet(1);
+            // получим все строки в файле
+            var rows = worksheet.RangeUsed().RowsUsed().Skip(1); // Skip header row
+            // пример чтения строк файла.
+            var count = rows.Count();
+            Recipient recipient;
+            foreach (var row in rows)
+            {
+                var id = RecipentList.Recipients.Count + 1;
+                recipient = new Recipient
+                {
+                    Name = row.Cell(2).Value.ToString(),
+                    Address = row.Cell(8).Value.ToString(),
+                    Company = row.Cell(4).Value.ToString(),
+                    Id = id,
+                    INN = row.Cell(6).Value.ToString(),
+                    Occupation = row.Cell(5).Value.ToString(),
+                    Phone = row.Cell(7).Value.ToString(),
+                    Position = row.Cell(3).Value.ToString(),
+                    WasSent = row.Cell(1).Value.ToString() == "+"
+                };
+                if(!RecipentList.Recipients.Contains(recipient)) RecipentList.Recipients.Add(recipient);
+            }
+
+            worksheet = workbook.Worksheet(2);
+
+            rows = worksheet.RangeUsed().RowsUsed();
+            count = rows.Count();
+            foreach (var row in rows)
+            {
+                var id = RecipentList.Recipients.Count + 1;
+                recipient = new Recipient
+                {
+                    Id = id,
+                    Name = row.Cell(1).Value.ToString(),
+                    Address = row.Cell(2).Value.ToString()
+                };
+
+                if (!RecipentList.Recipients.Contains(recipient)) RecipentList.Recipients.Add(recipient);
+            }
+
+            try
+            {
+                RecipentList.Save(fileSave);
+            }
+            catch (Exception e)
+            {
+                
+            }
+            workbook.Dispose();
+        }
+        static void Main(string[] args)
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory,$"Data\\контакты.xlsx");
+            var fileSave = Path.Combine(Environment.CurrentDirectory, $"Data\\Recipients.info");
+            var directory = Path.Combine(Environment.CurrentDirectory, "Data");
+            if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+
+            ReadExcel(filePath, fileSave);
+        }
+    }
+}
